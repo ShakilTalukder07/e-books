50f2375d9ed2e34d5077ddb0a17fd3e2428fb03c
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-09 11:56:24 +0600

    complete

---
+++ src/components/Book/Book.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Book = () => {
+    return (
+        <div>
+            <h1>this page is for shoeing books</h1>
+        </div>
+    );
+};
+
+export default Book;
